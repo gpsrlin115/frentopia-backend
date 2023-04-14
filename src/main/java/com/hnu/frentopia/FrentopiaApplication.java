fbd4b9331cbef90ab2609f9de03374accb60277d
gpsrlin115 <lefd123@gmail.com>
2023-04-14 18:55:04 +0900

    first commit

---
+++ src/main/java/com/hnu/frentopia/FrentopiaApplication.java
@@ -0,0 +1,13 @@
+package com.hnu.frentopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrentopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrentopiaApplication.class, args);
+	}
+
+}
