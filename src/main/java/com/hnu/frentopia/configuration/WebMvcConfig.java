f23655446b565af956d043dd728bb27c6421bc5b
ShimSeoHyun <ssh100206@gmail.com>
2023-04-29 21:16:29 +0900

    Change

--- src/main/java/com/hnu/frentopia/configuration/WebMvcConfig.java
+++ src/main/java/com/hnu/frentopia/configuration/WebMvcConfig.java
@@ -1,10 +1,6 @@
 package com.hnu.frentopia.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
--- src/main/java/com/hnu/frentopia/configuration/WebSecurityConfig.java
+++ src/main/java/com/hnu/frentopia/configuration/WebSecurityConfig.java
@@ -3,9 +3,7 @@ package com.hnu.frentopia.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
